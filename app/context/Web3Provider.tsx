fa611f8159d4b7e86bacd7386d9bc38929a7dca0
lucidartdev <lucidartdev@gmail.com>
2025-12-01 21:06:08 +0000

    feat: add web3 context provider

---
+++ app/context/Web3Provider.tsx
@@ -0,0 +1,10 @@
+import React, { ReactNode } from 'react';
+import '../config/reown';
+
+interface Props {
+  children: ReactNode;
+}
+
+export function Web3Provider({ children }: Props) {
+  return <>{children}</>;
+}
